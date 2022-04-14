0f8335237cefc40034e5e7a4e3b6d94917dc627d
Holly Henske <hcover333@gmail.com>
2022-04-14 07:52:52 -0500

    project finished except for readme

--- script.js
+++ script.js
@@ -1,41 +1,24 @@
 document.querySelectorAll('.accordian-question').forEach((item) => {
     item.addEventListener('click', (event) => {
-        console.log('click!')
-
         let accCollapse = item.nextElementSibling;
-
 
         if (!item.classList.contains('collapsing')) {
             if (!item.classList.contains('open')) {
-                
-                accCollapse.style.display = 'block'
-                let accHeight = accCollapse.clientHeight;
+                accCollapse.classList = 'accordian-collapse collapsing';
 
                 setTimeout(() => {
+                    accCollapse.classList = 'accordian-collapse open';
                     accCollapse.style.height = accHeight + 'px';
                     accCollapse.style.display = '';
-                }, 1);
-                
-
-                accCollapse.classList = 'accordian-collapse collapsing';
-                
-                setTimeout(() => {
-                    accCollapse.classList = 'accordian-collapse collapse open';
-                    // accCollapse.style.height = '';
                 }, 300);
             }
-            else {
-                accCollapse.classList = 'accordian-collapse collapsing';
+        else {
+            accCollapse.classList = 'accordian-collapse collapsing';
 
-                setTimeout(() => {
-                    accCollapse.style.height = '0px';
-                }, 1);
-
-                setTimeout(() => {
-                    accCollapse.classList = 'accordian-collapse collapse';
-                    accCollapse.style.height= ''
+            setTimeout(() => {
+   
+               accCollapse.classList = 'accordian-collapse collapse';
                 }, 300);
-                
             }
         }
         item.classList.toggle('open');
